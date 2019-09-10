9f05f5b6525960df35ca2ee3df440d8bed93e0d0
LordHoboDojo <32212717+LordHoboDojo@users.noreply.github.com>
2019-09-09 21:04:46 -0500

    File Reader

--- src/Confused.java
+++ src/Confused.java
@@ -1,12 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Confused {
-   static ArrayStack<String> stack = new ArrayStack<>();
+   private static ArrayStack<String> stack = new ArrayStack<>();
     public static void main(String[] args) throws Exception {
 
-        String str = "[([([]())])]";
-        System.out.println(isValid(str));
+        Scanner sc = new Scanner(new File("Confused.dat"));
+        int numberOfCases = sc.nextInt();
+        for (int i=0; i< numberOfCases; i++)
+        {
+            System.out.println(isValid(sc.nextLine()));
+        }
 
     }
-    public static boolean isValid(String s) throws Exception {
+    private static boolean isValid(String s) throws Exception {
         String[] strCharArray = s.split("");
         for (String str: strCharArray) {
             if (str.equals("(") || str.equals("[")) stack.push(str);
@@ -18,8 +25,6 @@ public class Confused {
             {
                 return false;
             }
-
-
         }
         return stack.isEmpty();
     }
